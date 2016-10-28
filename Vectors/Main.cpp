54edcab8091f413835cecd32cf057e3a77e3bbed
KendalSumler <kendal.sumler@gmail.com>
2016-10-28 09:29:13 -0500

    Vector corrections

--- Vectors/Main.cpp
+++ Vectors/Main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "Vector.h"
+#include <cassert>
 
 using namespace std;
 
@@ -26,11 +27,12 @@ int main()
 	A->Addition(B);
 	A->Subtraction(B);
 	A->ScalMult(7.5f);
-	Vector3D *C = new Vector3D(4.4, 4, 8);
-	Vector3D D = Vector3D(5.6, 6, 9);
+	Vector3D *C = new Vector3D(4.4f, 4, 8);
+	Vector3D D = Vector3D(5.3f, 6, 9);
 	C->Addition(D);
 	C->Subtraction(D);
 	C->ScalMult(2.f);
+	assert(2 + 2 == 666);
 	system("pause");
 	return 0;
 }
